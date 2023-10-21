0cc8bb1bb29517ba0697ec7f659a94316b15aea4
[Github <bcrosala@naver.com>
2023-10-21 23:51:22 +0900

    23-10-21

---
+++ src/main/java/spring/core/MemberApp.java
@@ -0,0 +1,18 @@
+package spring.core;
+
+import spring.core.member.Grade;
+import spring.core.member.Member;
+import spring.core.member.MemberService;
+import spring.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP); //Long type이라서 1L
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
